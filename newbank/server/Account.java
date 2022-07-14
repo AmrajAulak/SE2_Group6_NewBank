70d6d859f0aca86e52a6e9e68afc6aec929f6ecf
Amraj Aulak <aa754@bath.ac.uk>
2022-07-14 11:16:48 +0100

    Add account number functionality

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -1,17 +1,21 @@
 package newbank.server;
+import java.util.Random;
 
 public class Account {
 	
 	private String accountName;
 	private double openingBalance;
+	private long accountNumber;
 
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
 		this.openingBalance = openingBalance;
+		Random rand = new Random();
+		this.accountNumber = Math.round(rand.nextFloat()*1e6);
 	}
 	
 	public String toString() {
-		return (accountName + ": " + openingBalance);
+		return (accountNumber + " " + accountName + ": " + openingBalance + "\n");
 	}
 
 }
