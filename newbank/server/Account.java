fb25304292296177a0675699bfc9c29c25f1d82f
Amraj Aulak <aa754@bath.ac.uk>
2022-07-14 18:57:33 +0100

    Added functionality allowing customers to move funds between their accounts.

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -4,18 +4,35 @@ import java.util.Random;
 public class Account {
 	
 	private String accountName;
-	private double openingBalance;
+	private double balance;
 	private long accountNumber;
 
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
-		this.openingBalance = openingBalance;
+		this.balance = openingBalance;
 		Random rand = new Random();
 		this.accountNumber = Math.round(rand.nextFloat()*1e6);
 	}
 	
 	public String toString() {
-		return (accountNumber + " " + accountName + ": " + openingBalance + "\n");
+		return (accountName + ": " + balance + "\n");
+	}
+
+	public String getAccountName () {
+		return accountName;
+	}
+
+	public Boolean deductBalance(double amount){
+		if ((balance-amount) > 0) {
+			balance = balance - amount;
+			return true;
+		} else{
+			return false;
+		}
+	}
+
+	public void addBalance (double amount){
+		balance = balance + amount;
 	}
 
 }
